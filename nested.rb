ff5ddae2138c5208177d21a098d7f3d830f33942
Scoti Dodson <scotidodson@gmail.com>
2018-09-06 19:01:13 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -56,7 +56,7 @@ def dennis_ritchies_language
         }
      }
      dennis_ritchies_language = programmer_hash[:dennis_ritchie][:languages]
-     dennis_ritchies_language
+     puts dennis_ritchies_language
 end
 
 def adding_matz
